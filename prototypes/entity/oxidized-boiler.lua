5acf53c80b892d350d515aa09afbdcd834235418
Nicholas Gower <nicholasgower01@gmail.com>
2024-12-24 22:04:08 -0800

    feat: Added Space boiler(entity only), which consumes oxidizer instead of water and can operate in Space and on Muluna.

---
+++ prototypes/entity/oxidized-boiler.lua
@@ -0,0 +1,23 @@
+data_util=require("__flib__.data-util")
+
+local water_capacity=data.raw["fluid"]["water"].heat_capacity
+
+local heat,str=data_util.get_energy_value(water_capacity)
+
+data.raw["fluid"]["thruster-oxidizer"].heat_capacity=tostring(heat/15) .. str
+data.raw["fluid"]["thruster-oxidizer"].default_temperature=data.raw["fluid"]["water"].default_temperature
+--data.raw["fluid"]["thruster-fuel"].fuel_value=
+--data.raw["fluid"]["thruster-fuel"].fuel_value=data.raw["fluid"]["thruster-fuel"].fuel_value+data.raw["fluid"]["thruster-oxidizer"].fuel_value
+data.raw["fluid"]["thruster-oxidizer"].fuel_value=nil
+
+
+
+
+local space_boiler=table.deepcopy(data.raw["boiler"]["boiler"])
+space_boiler.name="space-boiler"
+space_boiler.fluid_box.filter="thruster-oxidizer"
+--space_boiler.energy_source.effectivity=64/60
+space_boiler.energy_consumption = "1.8MW",
+
+
+data:extend{space_boiler}
