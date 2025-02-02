31a1ca927adb8b1d971d560e53d3ab82584ee74c
Nicholas Gower <nicholasgower01@gmail.com>
2025-02-02 14:10:27 -0800

    fix: Fixed crash when fluid-nutrients not loaded.

--- prototypes/entity/cryolab.lua
+++ prototypes/entity/cryolab.lua
@@ -4,7 +4,7 @@ local cryolab=table.deepcopy(data.raw["lab"]["biolab"])
 cryolab.name="cryolab"
 cryolab.selection_box=flib_bounding_box.resize(cryolab.selection_box,1)
 cryolab.collision_box=flib_bounding_box.resize(cryolab.collision_box,1)
-if cryolab.energy_source then
+if mods["fluid-nutrients"] then
   cryolab.energy_source = { --Copied and modified from fluid-nutrients to make this mod compatible with its changes to biolabs..
     type = "fluid",
     fluid_box = {
