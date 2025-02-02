5c2303d4389f53c71a3edb7168fe542a170117a5
Nicholas Gower <nicholasgower01@gmail.com>
2025-02-02 00:39:35 -0800

    compat: Adjusted pollution rate of red steam engines to make using them as a pollution release more balanced when using them with advanced boilers.

--- compat/corrundum.lua
+++ compat/corrundum.lua
@@ -10,6 +10,10 @@ if data.raw["technology"]["planet-discovery-corrundum"] then
         }
     end
 end
+
+if data.raw["generator"]["red-steam-engine"] then 
+    data.raw["generator"]["red-steam-engine"].energy_source.emissions_per_minute.pollution = 20
+end
 -- if data.raw["technology"]["platinum-thruster"] then
 --     rro.soft_insert(data.raw["technology"]["platinum-thruster"].prerequisites,"interstellar-science-pack")
 -- end
