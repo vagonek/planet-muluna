427e1a500c2e98a9abd495adb755c644e0ddc3a2
Nicholas Gower <nicholasgower01@gmail.com>
2025-02-23 14:12:24 -0800

    compat: Updated surface conditions of AAI industry entities.

--- compat/aai-industry.lua
+++ compat/aai-industry.lua
@@ -1,27 +1,31 @@
 local rro = require("lib.remove-replace-object")
 local dual_icon = require("lib.dual-item-icon").dual_icon
+
+local one_oxygen_condition = {
+    property = "oxygen",
+    min = 1,
+    }
+
+
 if mods["aai-industry"] then
-    if data.raw["burner-assembling-machine"] then
-        data.raw["burner-assembling-machine"].surface_conditions = {
-            {
-            property = "oxygen",
-            min = 1,
-            }
-        }
+    if data.raw["assembling-machine"]["burner-assembling-machine"] then
+        PlanetsLib.restrict_surface_conditions(data.raw["assembling-machine"]["burner-assembling-machine"] , one_oxygen_condition)
     end
     
-    if data.raw["burner-lab"] then
-        table.insert(data.raw["burner-assembling-machine"].surface_conditions , {
-            property = "oxygen",
-            min = 1,
-        })
+    if data.raw["lab"]["burner-lab"] then
+        PlanetsLib.restrict_surface_conditions(data.raw["lab"]["burner-lab"] , one_oxygen_condition)
     end
     
-    if data.raw["burner-turbine"] then
-        table.insert(data.raw["burner-turbine"].surface_conditions , {
-            property = "oxygen",
-            min = 1,
-        })
+    if data.raw["generator"]["burner-turbine"] then
+        PlanetsLib.restrict_surface_conditions(data.raw["generator"]["burner-turbine"] , one_oxygen_condition)
+    end
+
+    if data.raw["mining-drill"]["burner-mining-drill"] then
+        PlanetsLib.restrict_surface_conditions(data.raw["mining-drill"]["burner-mining-drill"],one_oxygen_condition)
+    end
+
+    if data.raw["inserter"]["burner-inserter"] then
+        PlanetsLib.restrict_surface_conditions(data.raw["inserter"]["burner-inserter"],one_oxygen_condition)
     end
     -- rro.replace(data.raw["electric-engine-from-carbon"].ingredients ,
     -- {
@@ -37,23 +41,30 @@ data.raw["recipe"]["electric-engine-unit-from-carbon"].icons = dual_icon("electr
 
 local small_electric_motor = table.deepcopy(data.raw["recipe"]["electric-motor"])
 
-small_electric_motor = rro.merge(small_electric_motor, 
-{
-    name = "small-electric-motor-from-aluminum",
-    icon = nil,
-    icons = dual_icon("electric-motor","aluminum-cable"),
-    --localised_name = {"recipe-name.n-from-n",{"item-name.electric-motor"},{"item-name.aluminum-cable"}}
-})
+-- small_electric_motor = rro.merge(small_electric_motor, 
+-- {
+--     name = "small-electric-motor-from-aluminum",
+--     icon = nil,
+--     icons = dual_icon("electric-motor","aluminum-cable"),
+--     --localised_name = {"recipe-name.n-from-n",{"item-name.electric-motor"},{"item-name.aluminum-cable"}}
+-- })
 
-rro.replace(small_electric_motor.ingredients, {type = "item", name = "copper-cable", amount = 6}, {type = "item", name = "aluminum-cable", amount = 6})
+-- rro.replace(small_electric_motor.ingredients, {type = "item", name = "copper-cable", amount = 6}, {type = "item", name = "aluminum-cable", amount = 6})
 
-data:extend{small_electric_motor}
+-- data:extend{small_electric_motor}
+
+local burner_items = {
+    "burner-assembling-machine",
+    ""
+}
 
 
 
-table.insert(data.raw["technology"]["anorthite-processing"].effects,
-{
-    type = "unlock-recipe",
-    recipe = "small-electric-motor-from-aluminum",
-})
+
+
+-- table.insert(data.raw["technology"]["anorthite-processing"].effects,
+-- {
+--     type = "unlock-recipe",
+--     recipe = "small-electric-motor-from-aluminum",
+-- })
 end
