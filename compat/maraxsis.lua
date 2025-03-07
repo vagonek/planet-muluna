559702bbd9109be195525a381b61257859aba484
Nicholas Gower <nicholasgower01@gmail.com>
2025-03-07 10:44:41 -0800

    fix: Fixed crash when Maraxsis is not installed https://github.com/nicholasgower/planet-muluna/issues/104

--- compat/maraxsis.lua
+++ compat/maraxsis.lua
@@ -1,3 +1,4 @@
 if mods["maraxsis"] then 
+    PlanetsLib.restrict_surface_conditions(data.raw["recipe"]["maraxsis-hydrolox-rocket-fuel"], {property = "pressure", min = 200000})
     --table.insert(data.raw["technology"]["planet-discovery-maraxsis"].prerequisites,"interstellar-science-pack")
 end
