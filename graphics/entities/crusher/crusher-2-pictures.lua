cad172b732259e82f407120b80a216f7b5d92d4b
Nicholas Gower <nicholasgower01@gmail.com>
2025-03-19 15:06:03 -0700

    fix: Changed Crusher 2's working_visualisations to list to comply with 2.0.42's more stringent enforcement of working_visualisation's structure. https://forums.factorio.com/127608

--- graphics/entities/crusher/crusher-2-pictures.lua
+++ graphics/entities/crusher/crusher-2-pictures.lua
@@ -120,7 +120,7 @@ return {
     }
   },
   working_visualisations = {
-    layers = {
+    {
       fadeout = true,
       north_animation = crusher_working_visualisations_vertical(),
       east_animation = crusher_working_visualisations_horizontal(),
