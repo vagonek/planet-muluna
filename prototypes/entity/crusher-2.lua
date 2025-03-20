6adaf8e31daa1104a2f6fd78c28926b24b747694
Nicholas Gower <nicholasgower01@gmail.com>
2025-03-19 22:38:52 -0700

    balance: Raised HP of Crusher 2 by 50, and changed module slots of Crusher 2 to be 2 greater than that of Crusher 1.

--- prototypes/entity/crusher-2.lua
+++ prototypes/entity/crusher-2.lua
@@ -1,12 +1,14 @@
 local flib_data_util=require("__flib__.data-util")
 
+
 local crusher_2=table.deepcopy(data.raw["assembling-machine"]["crusher"])
 
 crusher_2.name="crusher-2"
-crusher_2.module_slots=4
+crusher_2.module_slots=crusher_2.module_slots + 2
 crusher_2.minable.result="crusher-2"
 crusher_2.graphics_set = require("__planet-muluna__.graphics.entities.crusher.crusher-2-pictures")
 crusher_2.crafting_speed=crusher_2.crafting_speed*1.5
+crusher_2.max_health=crusher_2.max_health+50
 local energy, suffix=flib_data_util.get_energy_value(crusher_2.energy_usage)
 crusher_2.energy_usage=tostring(energy*2) .. suffix
 
