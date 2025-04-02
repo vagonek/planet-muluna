f1c19ac3599f9352366281671309f81cdd7c98ad
Nicholas Gower <nicholasgower01@gmail.com>
2025-04-02 15:47:26 -0700

    fix: Removed non-standard character from file that caused issues with unzipping the mod on a Mac.

--- prototypes/sound/music.lua
+++ prototypes/sound/music.lua
@@ -6,7 +6,7 @@ if settings.startup["disable-muluna-music"].value == false then
         track_type = "main-track",
         planet = "muluna",
         sound = {
-            filename = "__planet-muluna__/sound/music/1 - Sandy Boys ⧸ Beth Cohen's [aiqbWEikr4w].ogg",
+            filename = "__planet-muluna__/sound/music/1 - Sandy Boys Beth Cohen's [aiqbWEikr4w].ogg",
             volume = 0.5,
         }
     }
