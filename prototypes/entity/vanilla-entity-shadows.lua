59cfe00a11079f4e28db3eba4c57e820370f1dd6
Nicholas Gower <nicholasgower01@gmail.com>
2025-04-10 13:41:27 -0700

    graphics: Created shadow-separated version of "pipe to ground" entity, allowing deeper shadows to be visible on Muluna on this entity.

---
+++ prototypes/entity/vanilla-entity-shadows.lua
@@ -0,0 +1,47 @@
+
+
+local pipe_ground = table.deepcopy(data.raw["pipe-to-ground"]["pipe-to-ground"])
+local new_pictures = {}
+for key,picture in pairs(pipe_ground.pictures) do
+    local old_picture = table.deepcopy(pipe_ground.pictures[key])
+    --local new_layers = {old_picture}
+    -- old_picture.filename = __base__/graphics/entity/pipe-to-ground/pipe-to-ground-up.png
+    local new_filename_shadow = string.gsub(old_picture.filename,"__base__/graphics/entity","__muluna-graphics__/graphics/entities")
+    local new_filename = string.gsub(old_picture.filename,"__base__/graphics","__muluna-graphics__/graphics")
+    local new_filename_shadow = string.gsub(new_filename,"/pipe%-to%-ground/","/pipe-to-ground-shadow/")
+    log(new_filename)
+    log(new_filename_shadow)
+    assert(new_filename ~= old_picture.filename,new_filename)
+    assert(new_filename_shadow ~= new_filename,new_filename_shadow)
+    local new_layers = {
+        {
+            filename = new_filename,
+            priority = "extra-high",
+            width = 128,
+            height = 128,
+            scale = 0.5,
+          },
+    {
+        filename = new_filename_shadow,
+        priority = "extra-high",
+        width = 128,
+        height = 128,
+        scale = 0.5,
+        draw_as_shadow = true
+      }
+    }
+    
+      new_pictures[key] = {layers = new_layers}
+end
+
+pipe_ground.pictures = {
+    north = {
+
+    }
+}
+
+
+pipe_ground.pictures = new_pictures
+
+
+data:extend{pipe_ground}
