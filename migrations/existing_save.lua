0b74d4f96a44d643ab93dffe584aafb2a723f044
Nicholas Gower <nicholasgower01@gmail.com>
2025-04-11 01:08:54 -0700

    fix: Made existing_save migration more strict to avoid triggering it on saves with Muluna already installed.

--- migrations/existing_save.lua
+++ migrations/existing_save.lua
@@ -1,39 +1,41 @@
 for _,force in pairs(game.forces) do 
-    if force.technologies["space-science-pack"].researched == true then
-        force.technologies["crusher"].researched = true
-        force.technologies["muluna-advanced-boiler"].researched = true
-        force.technologies["planet-discovery-muluna"].researched = true
-        force.technologies["greenhouses"].researched = true
-    end
+    if force.technologies["planet-discovery-muluna"].researched == false then
+        if force.technologies["space-science-pack"].researched == true then
+            force.technologies["crusher"].researched = true
+            force.technologies["muluna-advanced-boiler"].researched = true
+            force.technologies["planet-discovery-muluna"].researched = true
+            force.technologies["greenhouses"].researched = true
+        end
 
-    if force.technologies["space-platform-thruster"].researched == true then
-        force.technologies["asteroid-collector"].researched = true
-        force.technologies["muluna-anorthite-processing"].researched = true
-        force.technologies["rocket-part-productivity"].researched = true
-        force.technologies["rocket-part-productivity-2"].researched = true
-        force.technologies["rocket-part-productivity-3"].researched = true
-        force.technologies["rocket-part-productivity-4"].researched = true
-    end
+        if force.technologies["space-platform-thruster"].researched == true then
+            force.technologies["asteroid-collector"].researched = true
+            force.technologies["muluna-anorthite-processing"].researched = true
+            force.technologies["rocket-part-productivity"].researched = true
+            force.technologies["rocket-part-productivity-2"].researched = true
+            force.technologies["rocket-part-productivity-3"].researched = true
+            force.technologies["rocket-part-productivity-4"].researched = true
+        end
 
-    if force.technologies["electromagnetic-science-pack"].researched == true then
-        force.technologies["rocket-part-productivity-fulgora"].researched = true
-        force.technologies["rocket-part-productivity-fulgora-2"].researched = true
-    end
+        if force.technologies["electromagnetic-science-pack"].researched == true then
+            force.technologies["rocket-part-productivity-fulgora"].researched = true
+            force.technologies["rocket-part-productivity-fulgora-2"].researched = true
+        end
 
-    if force.technologies["agricultural-science-pack"].researched == true then
-        force.technologies["rocket-part-productivity-gleba"].researched = true
-        force.technologies["rocket-part-productivity-gleba-2"].researched = true
-    end
+        if force.technologies["agricultural-science-pack"].researched == true then
+            force.technologies["rocket-part-productivity-gleba"].researched = true
+            force.technologies["rocket-part-productivity-gleba-2"].researched = true
+        end
 
-    if force.technologies["metallurgic-science-pack"].researched == true then
-        force.technologies["rocket-part-productivity-vulcanus"].researched = true
-        force.technologies["rocket-part-productivity-vulcanus-2"].researched = true
-    end
+        if force.technologies["metallurgic-science-pack"].researched == true then
+            force.technologies["rocket-part-productivity-vulcanus"].researched = true
+            force.technologies["rocket-part-productivity-vulcanus-2"].researched = true
+        end
 
-    if force.technologies["planet-discovery-aquilo"].researched == true then
-        force.technologies["interstellar-science-pack"].researched = true
-        force.technologies["advanced-space-science-pack"].researched = true
-        force.technologies["thruster-fuel-productivity"].researched = true
-        force.technologies["thruster-fuel-productivity"].level = 6
+        if force.technologies["planet-discovery-aquilo"].researched == true then
+            force.technologies["interstellar-science-pack"].researched = true
+            force.technologies["advanced-space-science-pack"].researched = true
+            force.technologies["thruster-fuel-productivity"].researched = true
+            force.technologies["thruster-fuel-productivity"].level = 6
+        end
     end
 end
