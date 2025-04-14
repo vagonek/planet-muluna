9a9e47f93bd98c45a46dedac454093e8fce4dafb
Nicholas Gower <nicholasgower01@gmail.com>
2025-04-14 11:36:06 -0700

    fix: Reenabled improved inserter shaders when Artisanal library is loaded.

--- prototypes/entity/vanilla-entity-shadows.lua
+++ prototypes/entity/vanilla-entity-shadows.lua
@@ -1,88 +1,23 @@
-if settings.startup["muluna-separate-shadows"].value == true and not(mods["reskins-library"]) then
+if settings.startup["muluna-separate-shadows"].value == true then 
     local rro = require("lib.remove-replace-object")
 
-    local pipe_ground = table.deepcopy(data.raw["pipe-to-ground"]["pipe-to-ground"])
-    local new_pictures = {}
-    local send_new_pictures = true
-    for key,picture in pairs(pipe_ground.pictures) do
-        local old_picture = table.deepcopy(pipe_ground.pictures[key])
-        --local new_layers = {old_picture}
-        -- old_picture.filename = __base__/graphics/entity/pipe-to-ground/pipe-to-ground-up.png
-        --local new_filename_shadow = string.gsub(old_picture.filename or old_picture.layers[1].filename,"__.+__/graphics/entity","__muluna-graphics__/graphics/entities")
-        local new_filename = string.gsub(old_picture.filename or old_picture.layers[1].filename,"__.+__/graphics","__muluna-graphics__/graphics")
-        local new_filename_shadow = new_filename
-        if string.find(new_filename,"__muluna%-graphics__") then
-            new_filename_shadow = string.gsub(new_filename,"/pipe%-to%-ground/","/pipe-to-ground-shadow/")
-        end
-        
-        --log(new_filename)
-        --log(new_filename_shadow)
-        --assert(new_filename ~= old_picture.filename,new_filename)
-        --assert(new_filename_shadow ~= new_filename,new_filename_shadow)
-        if new_filename ~= old_picture.filename and new_filename_shadow ~= new_filename then
-            local new_layers = {
-                {
-                    filename = new_filename,
-                    priority = "extra-high",
-                    width = 128,
-                    height = 128,
-                    scale = 0.5,
-                },
-            {
-                filename = new_filename_shadow,
-                priority = "extra-high",
-                width = 128,
-                height = 128,
-                scale = 0.5,
-                draw_as_shadow = true
-            }
-            }
-            
-            new_pictures[key] = {layers = new_layers}
-        else
-            send_new_pictures = false
-        end
-
-    end
-
-
-    if send_new_pictures == true then
-        pipe_ground.pictures = new_pictures
-    end
-
-
-
-    data:extend{pipe_ground}
-
-    local pipe = table.deepcopy(data.raw["pipe"]["pipe"])
-    local new_pictures = {}
-    local send_new_pictures = true
-    for key,picture in pairs(pipe.pictures) do
-        local old_picture = table.deepcopy(pipe.pictures[key])
-        --local new_layers = {old_picture}
-        -- old_picture.filename = __base__/graphics/entity/pipe-to-ground/pipe-to-ground-up.png
-        --local new_filename_shadow = string.gsub(old_picture.filename,"__base__/graphics/entity","__muluna-graphics__/graphics/entities")
-        local excluded_keys = {
-            "horizontal_window_background",
-            "vertical_window_background",
-            "fluid_background",
-            "low_temperature_flow",
-            "middle_temperature_flow",
-            "high_temperature_flow",
-            "gas_flow",
-
-        }
-        if rro.contains(excluded_keys,key) or string.find(key,"frozen") or string.find(key,"visualization") then
-            new_pictures[key] = old_picture
-        else
-            local new_filename = string.gsub(old_picture.filename or old_picture.layers[1].filename,"__base__/graphics","__muluna-graphics__/graphics")
-            local new_filename_shadow = new_filename 
-            if string.find(new_filename_shadow,"__muluna-graphics__") then
-                new_filename_shadow = string.gsub(new_filename,"/pipe/","/pipe-shadow/")
+    if not reskins then --Reskins-library already replaces these assets
+        local pipe_ground = table.deepcopy(data.raw["pipe-to-ground"]["pipe-to-ground"])
+        local new_pictures = {}
+        local send_new_pictures = true
+        for key,picture in pairs(pipe_ground.pictures) do
+            local old_picture = table.deepcopy(pipe_ground.pictures[key])
+            --local new_layers = {old_picture}
+            -- old_picture.filename = __base__/graphics/entity/pipe-to-ground/pipe-to-ground-up.png
+            --local new_filename_shadow = string.gsub(old_picture.filename or old_picture.layers[1].filename,"__.+__/graphics/entity","__muluna-graphics__/graphics/entities")
+            local new_filename = string.gsub(old_picture.filename or old_picture.layers[1].filename,"__.+__/graphics","__muluna-graphics__/graphics")
+            local new_filename_shadow = new_filename
+            if string.find(new_filename,"__muluna%-graphics__") then
+                new_filename_shadow = string.gsub(new_filename,"/pipe%-to%-ground/","/pipe-to-ground-shadow/")
             end
             
-            log(new_filename)
-            log(new_filename_shadow)
+            --log(new_filename)
+            --log(new_filename_shadow)
             --assert(new_filename ~= old_picture.filename,new_filename)
             --assert(new_filename_shadow ~= new_filename,new_filename_shadow)
             if new_filename ~= old_picture.filename and new_filename_shadow ~= new_filename then
@@ -108,18 +43,85 @@ if settings.startup["muluna-separate-shadows"].value == true and not(mods["reski
             else
                 send_new_pictures = false
             end
+
         end
 
+
+        if send_new_pictures == true then
+            pipe_ground.pictures = new_pictures
+        end
+
+
+
+        data:extend{pipe_ground}
+
+        local pipe = table.deepcopy(data.raw["pipe"]["pipe"])
+        local new_pictures = {}
+        local send_new_pictures = true
+        for key,picture in pairs(pipe.pictures) do
+            local old_picture = table.deepcopy(pipe.pictures[key])
+            --local new_layers = {old_picture}
+            -- old_picture.filename = __base__/graphics/entity/pipe-to-ground/pipe-to-ground-up.png
+            --local new_filename_shadow = string.gsub(old_picture.filename,"__base__/graphics/entity","__muluna-graphics__/graphics/entities")
+            local excluded_keys = {
+                "horizontal_window_background",
+                "vertical_window_background",
+                "fluid_background",
+                "low_temperature_flow",
+                "middle_temperature_flow",
+                "high_temperature_flow",
+                "gas_flow",
+
+            }
+            if rro.contains(excluded_keys,key) or string.find(key,"frozen") or string.find(key,"visualization") then
+                new_pictures[key] = old_picture
+            else
+                local new_filename = string.gsub(old_picture.filename or old_picture.layers[1].filename,"__base__/graphics","__muluna-graphics__/graphics")
+                local new_filename_shadow = new_filename 
+                if string.find(new_filename_shadow,"__muluna%-graphics__") then
+                    new_filename_shadow = string.gsub(new_filename,"/pipe/","/pipe-shadow/")
+                end
+                
+                log(new_filename)
+                log(new_filename_shadow)
+                --assert(new_filename ~= old_picture.filename,new_filename)
+                --assert(new_filename_shadow ~= new_filename,new_filename_shadow)
+                if new_filename ~= old_picture.filename and new_filename_shadow ~= new_filename then
+                    local new_layers = {
+                        {
+                            filename = new_filename,
+                            priority = "extra-high",
+                            width = 128,
+                            height = 128,
+                            scale = 0.5,
+                        },
+                    {
+                        filename = new_filename_shadow,
+                        priority = "extra-high",
+                        width = 128,
+                        height = 128,
+                        scale = 0.5,
+                        draw_as_shadow = true
+                    }
+                    }
+                    
+                    new_pictures[key] = {layers = new_layers}
+                else
+                    send_new_pictures = false
+                end
+            end
+
+        end
+
+
+        if send_new_pictures == true then
+            pipe.pictures = new_pictures
+        end
+
+
+
+        data:extend{pipe}
     end
-
-
-    if send_new_pictures == true then
-        pipe.pictures = new_pictures
-    end
-
-
-
-    data:extend{pipe}
 
 --Inserters 
 
