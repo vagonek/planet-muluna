ecb96498a2aedd0fcdfec4a182ddefe3a0e08c66
Nicholas Gower <nicholasgower01@gmail.com>
2025-04-13 18:46:30 -0700

    fix: Fixed impossible start when loaded with Lignumis. https://github.com/nicholasgower/planet-muluna/issues/146

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -148,7 +148,7 @@ data.raw["recipe"]["copper-cable"].localised_name={"recipe-name.copper-cable"}
 require("compat.aai-industry")
 require("prototypes.technology.interstellar-science-pack-final-fix")
 require("prototypes.entity.vanilla-entity-shadows")
-if data.raw["technology"]["tree-seeding"] then --Removed vanilla/wood-gasification recipes from tree seeding, then deletes the tech if no other mods add recipes to the tech.
+if data.raw["technology"]["tree-seeding"] and not data.raw.planet.lignumis then --Removed vanilla/wood-gasification recipes from tree seeding, then deletes the tech if no other mods add recipes to the tech.
 --Technologies that have this tech as a prerequisite are moved to having agricultural science pack as the prerequisite.
     rro.remove(data.raw["technology"]["tree-seeding"].effects, {type = "unlock-recipe", recipe = "wood-processing"})
     rro.remove(data.raw["technology"]["tree-seeding"].effects, {type = "unlock-recipe", recipe = "wood-seed-greenhouse"})
