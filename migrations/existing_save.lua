0722f411a793f1440d50f64951b47de66963cb9d
Nicholas Gower <nicholasgower01@gmail.com>
2025-04-25 00:45:03 -0700

    More updates to tech tree

--- migrations/existing_save.lua
+++ migrations/existing_save.lua
@@ -1,15 +1,17 @@
 for _,force in pairs(game.forces) do 
     if force.technologies["planet-discovery-muluna"].researched == false then
         if force.technologies["space-science-pack"].researched == true then
-            force.technologies["crusher"].researched = true
-            force.technologies["muluna-advanced-boiler"].researched = true
+            --force.technologies["crusher"].researched = true
+            force.technologies["metallic-asteroid-crushing"].researched = true
+            force.technologies["carbonic-asteroid-crushing"].researched = true
+            force.technologies["oxide-asteroid-crushing"].researched = true
             force.technologies["planet-discovery-muluna"].researched = true
-            force.technologies["greenhouses"].researched = true
+
         end
 
         if force.technologies["space-platform-thruster"].researched == true then
             force.technologies["asteroid-collector"].researched = true
-            force.technologies["muluna-anorthite-processing"].researched = true
+            --force.technologies["muluna-anorthite-processing"].researched = true
             force.technologies["rocket-part-productivity"].researched = true
             force.technologies["rocket-part-productivity-2"].researched = true
             force.technologies["rocket-part-productivity-3"].researched = true
