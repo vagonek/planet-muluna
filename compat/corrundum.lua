b97b02d8a405cff21db6f25e8140690832466377
Nicholas Gower <nicholasgower01@gmail.com>
2025-05-03 11:28:50 -0700

    compat: Replaced icon for Corrundum's "Controlled petroleum combustion" recipe with icon that better fits in with Muluna's graphics for carbon dioxide.

--- compat/corrundum.lua
+++ compat/corrundum.lua
@@ -1,4 +1,5 @@
 local rro = require("lib.remove-replace-object")
+local dual_icon = require("lib.dual-item-icon").dual_icon
 if data.raw["technology"]["planet-discovery-corrundum"] then
     --rro.replace(data.raw["technology"]["planet-discovery-corrundum"].prerequisites,"space-science-pack","asteroid-collector")
     if data.raw["boiler"]["red-boiler"] then
@@ -14,6 +15,10 @@ end
 
 if data.raw["generator"]["red-steam-engine"] then 
     data.raw["generator"]["red-steam-engine"].energy_source.emissions_per_minute.pollution = 20
+end
+
+if data.raw["recipe"]["controlled-petrol-combustion"] then
+    data.raw["recipe"]["controlled-petrol-combustion"].icons = dual_icon("carbon-dioxide","petroleum-gas")
 end
 -- if data.raw["technology"]["platinum-thruster"] then
 --     rro.soft_insert(data.raw["technology"]["platinum-thruster"].prerequisites,"interstellar-science-pack")
